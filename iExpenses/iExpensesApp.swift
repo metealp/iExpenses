025a97141239b8e1a03f177198e8a52db56bf778
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-23 22:33:03 +0300

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Mete Alp Kizilcay on 23.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
