01eeffbd3f3e87533b818552b574455ee03f2afd
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-23 23:38:34 +0300

    add and list expense items

--- iExpenses/ContentView.swift
+++ iExpenses/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items) {
+                    item in
+                    HStack {
+                        VStack (alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpenses")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.showingAddExpense.toggle()
+                                    }) {
+                                        Image(systemName: "plus")
+                                    }
+            )
+            .sheet(isPresented: $showingAddExpense, content: {
+                AddView(expenses: self.expenses)
+            })
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
